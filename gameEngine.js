293bc6edaffa6563f5f06bdf405c0fcd6a3ea5d4
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2015-07-07 22:07:25 +0900

    edit

--- gameEngine.js
+++ gameEngine.js
@@ -177,7 +177,7 @@ function leftButtonDisable(){
 function checkDistance(target){
 	var y = honoka.img.y - target.img.y;
 
-	var length = Math.abs(y) - CAR1_IMG_HEIGHT*gameScreenScale*0.3 - HONOKA_IMG_HEIGHT*gameScreenScale*0.3;
+	var length = Math.abs(y) - CAR1_IMG_HEIGHT*gameScreenScale*0.25 - HONOKA_IMG_HEIGHT*gameScreenScale*0.25;
 	return length;
 }
 //イベント処理-------------------------------------
