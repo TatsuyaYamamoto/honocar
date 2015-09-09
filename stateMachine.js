7b8a9be95033111bbf159c8c6f0c5dcdddf3eea1
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2015-09-10 00:26:00 +0900

    edit

--- stateMachine.js
+++ stateMachine.js
@@ -108,8 +108,8 @@ function menuState(){
             }
         }).done(function(data){
             var pic = new createjs.Bitmap(data.profile_image_url);
-            setCoordinates(pic, (gameScrean.width)*0.05, gameScrean.height*0.9);
-            pic.scaleY = pic.scaleX = gameScreenScale*2;
+            setCoordinates(pic, (gameScrean.width)*0.03, gameScrean.height*0.9);
+            pic.scaleY = pic.scaleX = gameScreenScale * 1.8;
             gameStage.addChild(pic);
         }).fail(function(){
             alert("アクセスに失敗しました");
