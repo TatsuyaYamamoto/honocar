e40707d351cdbce81961103ec49e340f09e95caf
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2015-09-12 11:24:31 +0900

    edit twitter icon

--- common.js
+++ common.js
@@ -174,7 +174,7 @@ function checkLogin(){
         if (xhr.status === 200) {
             isLogin = true;
             screen_name = data.screen_name;
-            manifest.api[0].src = data.profile_image_url.replace("_nomal", "" );
+            manifest.api[0].src = data.profile_image_url.replace("_normal", "" );
             alert(manifest.api[0].src);
         }
     }).always(function(){
